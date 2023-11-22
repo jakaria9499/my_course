2592d8ae35c66e672a46e90e7d3b400a255a4f6d
jakaria mahmud <jakaria0499@mgail.com>
2023-11-22 13:03:15 +0600

    module 6 is complated

---
+++ c++ code/module1/input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{   
+    int a;
+    long long int b;
+    char c;
+    cin >> c;
+    // long long int c = (long long int)(a)
+    cout << int(c) << endl;
+
+
+    return 0;
+}
---
+++ c++ code/module1/print.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"jakaria mahmud"<<std::endl;
+    return 0;
+}
